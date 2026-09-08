df43cef1ca664ca37676c3a5e3b2d8029a855d5b
SteveParadox <fordstphn@gmail.com>
2026-09-08 17:42:07 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: './',
+  base: '/',
   plugins: [
     command === "serve" ? inspectAttr() : null,
     react(),
